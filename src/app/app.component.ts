d0ef56462e29a5ef766f21b9f8984629de1f4c3a
SELISE\Riad.ahmed <riad.zakir@selise.ch>
2019-01-06 10:39:21 +0600

    Theming added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'theme-test';
+  theme = '';
+
+  constructor() {
+    if (localStorage.getItem('selectedTheme') === null) {
+      localStorage.setItem('selectedTheme', 'first-theme');
+    } else {
+      this.theme = localStorage.getItem('selectedTheme');
+    }
+  }
+
+  changeTheme(theme) {
+    this.theme = theme;
+    localStorage.setItem('selectedTheme', this.theme);
+  }
 }
